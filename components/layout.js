a98c168b3f9786560123a28bde652638ec38adc7
soumi0705 <somida00@gmail.com>
2022-08-10 18:02:01 +0530

    Changes to Layouts and web3.js

---
+++ components/layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Menu } from "semantic-ui-react";
+import 'semantic-ui-css/semantic.min.css';
+import Header from "./Header";
+const Layout = (props) => {
+    return(
+        <Container>
+            <Header />
+            {props.children}
+        </Container>
+    );
+};
+
+export default Layout
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
 import factory from '../ethereum/factory'
-
+import {Card, Button} from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css'
+import Layout from "../components/layout";
 class CampaignIndex extends Component{
-    async componentDidMount(){
-        var campaigns = await factory.methods.getDeployedCampaigns().call();
+    static async getInitialProps() {
+        var campaigns = await factory.methods
+            .getDeployedCampaigns()
+            .call();
+        return {campaigns};
+        
+    }
 
-        console.log(campaigns);
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address =>{
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true,
+            }
+        });
+        return <Card.Group items={items} />
     }
 
     render(){
-        return <div>Campaigns Index!</div>
+        return( 
+        <Layout>
+        <div>
+        <h3>Open Campaigns</h3>
+        
+        <Button
+            content="Create Campaign"
+            icon="add circle"
+            primary
+            labelPosition="left"
+            floated="right"
+        />
+        {this.renderCampaigns()}
+        </div>
+        </Layout>        
+        )
     }
 }
 
