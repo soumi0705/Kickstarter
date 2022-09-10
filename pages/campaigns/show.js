762888fe8520aa0f2c2edf6f0280aface3da250a
soumi0705 <somida00@gmail.com>
2022-09-10 21:22:04 +0530

    Campaign and Request page completed

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,69 +1,123 @@
-import React , {Component} from 'react';
-import { Card } from 'semantic-ui-react';
-import Layout from '../../components/layout';
-import Campaign from '../../ethereum/campaign';
-import web3 from '../../ethereum/web3';
+import { Link } from "../../routes";
+import React, { Component } from "react";
+import { Button, Card, Grid, GridRow } from "semantic-ui-react";
+import ContributeForm from "../../components/ContributeForm";
+import Layout from "../../components/layout";
+import Campaign from "../../ethereum/campaign";
+import web3 from "../../ethereum/web3";
 
 class CampaignShow extends Component {
-    static async getInitialProps(props){
+  static async getInitialProps(props) {
+    const campaign = Campaign(props.query.address);
 
-        const campaign = Campaign(props.query.address);
-        
-        const summary = await campaign.methods.getSummary().call();
-        
-        return{
-            minimumContribution: summary[0],
-            balance: summary[1],
-            requestsCount: summary[3],
-            approversCount: summary[3],
-            manager: summary[4]
-        }
-    }
+    const summary = await campaign.methods.getSummary().call();
 
-    renderCards() {
-        const {balance,manager,minimumContribution,requestsCount,approversCount} = this.props;
-        const items = [
-            {
-                header: manager,
-                meta: 'Address of Manager',
-                description: 'The manager created this campaign and can create requests to withdraw money',
-                style: {
-                    overflowWrap: 'break-word'
-                }
-            },
-            {
-                header: minimumContribution,
-                meta: 'Minimum Contribution (wei)',
-                description: 'You must contribute at least this much wei to become an approver',
+    return {
+      address: props.query.address,
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4],
+    };
+  }
 
-            },
-            {
-                header: requestsCount,
-                meta: 'Number of Requests',
-                description: 'A request tries to withdraw money from the contract. Requests must be approved by the approvers',
-            },
-            {
-                header: approversCount,
-                meta: 'Number of Approvers',
-                description: 'Number of people who have already donated to this campaign',
-            },
-            {
-                header: web3.utils.fromWei(balance,'ether'),
-                meta:'Campaign Balance (ether)',
-                description: 'THe balance is how much money this campaign has left to spend'
-            }
-        ];
-        return (<Card.Group items={items}/>);
-    }
+  state = {
+    address: this.props.address,
+    minimumContribution: this.props.minimumContribution,
+    balance: this.props.balance,
+    requestsCount: this.props.requestsCount,
+    approversCount: this.props.approversCount,
+    manager: this.props.manager,
+  };
 
+  refreshPage = async () => {
+    const campaign = Campaign(this.state.address);
 
-    render() {
-        return (
-            <Layout>
-                {this.renderCards()}
-            </Layout>
-        );
-    }
+    const summary = await campaign.methods.getSummary().call();
+
+    this.setState({
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4],
+    });
+  };
+
+  renderCards() {
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      approversCount,
+    } = this.state;
+    const items = [
+      {
+        header: manager,
+        meta: "Address of Manager",
+        description:
+          "The manager created this campaign and can create requests to withdraw money",
+        style: {
+          overflowWrap: "break-word",
+        },
+      },
+      {
+        header: minimumContribution,
+        meta: "Minimum Contribution (wei)",
+        description:
+          "You must contribute at least this much wei to become an approver",
+      },
+      {
+        header: requestsCount,
+        meta: "Number of Requests",
+        description:
+          "A request tries to withdraw money from the contract. Requests must be approved by the approvers",
+      },
+      {
+        header: approversCount,
+        meta: "Number of Approvers",
+        description:
+          "Number of people who have already donated to this campaign",
+      },
+      {
+        header: web3.utils.fromWei(balance, "ether"),
+        meta: "Campaign Balance (ether)",
+        description:
+          "THe balance is how much money this campaign has left to spend",
+      },
+    ];
+    return <Card.Group items={items} />;
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h3>Campaign Show</h3>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
+            <Grid.Column width={6}>
+              <ContributeForm
+                refreshPage={this.refreshPage}
+                address={this.props.address}
+              />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Link route={`/campaigns/${this.state.address}/requests`}>
+                <a>
+                  <Button primary>View Requests</Button>
+                </a>
+              </Link>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Layout>
+    );
+  }
 }
 
 export default CampaignShow;
